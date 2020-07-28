59a35ea47cdd409e3a38c315a9b69b027986b238
Ivan Kopeykin <ikopeykin@ozon.ru>
2020-07-28 19:23:14 +0300

    add ImportsFieldPlugin

--- types.d.ts
+++ types.d.ts
@@ -110,6 +110,7 @@ declare interface ParsedIdentifier {
 	directory: boolean;
 	module: boolean;
 	file: boolean;
+	internal: boolean;
 }
 declare interface PnpApiImpl {
 	resolveToUnqualified: (arg0: string, arg1: string, arg2?: any) => string;
@@ -164,6 +165,7 @@ declare interface ResolveOptions {
 	descriptionFiles: (string)[];
 	enforceExtension: boolean;
 	exportsFields: Set<string | (string)[]>;
+	importsFields: Set<string | (string)[]>;
 	extensions: Set<string>;
 	fileSystem: FileSystem;
 	unsafeCache: any;
@@ -188,6 +190,7 @@ declare interface ResolveRequest {
 	fragment?: undefined | string;
 	directory?: undefined | boolean;
 	module?: undefined | boolean;
+	internal?: undefined | boolean;
 	descriptionFilePath?: undefined | string;
 	descriptionFileRoot?: undefined | string;
 	descriptionFileData?: any;
@@ -259,6 +262,7 @@ declare abstract class Resolver {
 	): any;
 	parse(identifier: string): ParsedIdentifier;
 	isModule(path?: any): boolean;
+	isPrivate(path?: any): boolean;
 	isDirectory(path: string): boolean;
 	join(path?: any, request?: any): string;
 	normalize(path?: any): string;
@@ -310,6 +314,11 @@ declare interface UserResolveOptions {
 	 * A list of exports fields in description files
 	 */
 	exportsFields?: undefined | (string | (string)[])[];
+
+	/**
+	 * A list of imports fields in description files
+	 */
+	importsFields?: undefined | (string | (string)[])[];
 
 	/**
 	 * A list of extensions which should be tried for files
