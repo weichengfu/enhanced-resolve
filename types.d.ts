c7896e38986802b1e34dde2a7f42206dbc45efb2
Ivan Kopeykin <ikopeykin@ozon.ru>
2020-08-27 19:20:35 +0300

    add fallback option
    - rework how AliasPlugin work
    - add fallback option and test cases

--- types.d.ts
+++ types.d.ts
@@ -162,6 +162,11 @@ declare interface ResolveOptions {
 		name: string;
 		onlyModule?: undefined | boolean;
 	})[];
+	fallback: ({
+		alias: string | false | (string)[];
+		name: string;
+		onlyModule?: undefined | boolean;
+	})[];
 	aliasFields: Set<string | (string)[]>;
 	cachePredicate: (
 		arg0: BaseResolveRequest & Partial<ParsedIdentifier>
@@ -270,6 +275,18 @@ declare interface UserResolveOptions {
 	 * A list of module alias configurations or an object which maps key to value
 	 */
 	alias?:
+		| undefined
+		| { [index: string]: string | false | (string)[] }
+		| ({
+				alias: string | false | (string)[];
+				name: string;
+				onlyModule?: undefined | boolean;
+		  })[];
+
+	/**
+	 * A list of module alias configurations or an object which maps key to value, applied only after modules option
+	 */
+	fallback?:
 		| undefined
 		| { [index: string]: string | false | (string)[] }
 		| ({
